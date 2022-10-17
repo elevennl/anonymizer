1ad1a1ee3df8ba9919369c10e35589803ffcb4c8
Rowan Ramtahalsing <rowan@eleven.nl>
2022-10-17 16:16:34 +0200

    (fix) rework after feedback

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,10 @@
 export {Client} from 'https://deno.land/x/mysql/mod.ts';
 export {faker} from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts';
 export {Rhum} from 'https://deno.land/x/rhum@v1.1.12/mod.ts';
+
+export type {ClientConfig} from 'https://deno.land/x/mysql@v2.10.2/src/client.ts';
+
+import ProgressModule from 'https://deno.land/x/progress@v1.2.3/mod.ts';
+
+export const Progress = ProgressModule;
+export type Progress = ProgressModule;
