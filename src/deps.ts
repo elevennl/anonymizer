2feb4c13ed683a32afc73c1c170e5b0d928aa9dd
Remco Dekker <remco@eleven.nl>
2022-11-09 17:42:32 +0100

    Refactored tests in helper.test.ts to use the built-in Deno test runner; Removing the Rhum dependency

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,14 @@
+export {assertEquals, assertThrows, assertObjectMatch} from 'https://deno.land/std@0.163.0/testing/asserts.ts';
+export {
+	afterEach,
+	beforeEach,
+	beforeAll,
+	describe,
+	it
+} from 'https://deno.land/std@0.163.0/testing/bdd.ts';
+
 export {Client} from 'https://deno.land/x/mysql@v2.10.3/mod.ts';
 export {faker} from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts';
-export {Rhum} from 'https://deno.land/x/rhum@v1.1.15/mod.ts';
 
 export type {ClientConfig} from 'https://deno.land/x/mysql@v2.10.3/src/client.ts';
 
