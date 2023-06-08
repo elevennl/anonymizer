35eacf93360110154b862a3799cfd91d3491d602
Patrick van Zadel <patrickvanzadel@eleven.nl>
2023-06-08 14:01:28 +0200

    (bug): During creation of the merge table an error occurs when a targeted table does not have a primary column named 'id'
    Added license header to all new files

--- src/database/anonimizer.ts
+++ src/database/anonimizer.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2022, Rowan Ramtahalsing <rowanramtahalsing@eleven.nl>
+ *
+ * Anonymizer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Anonymizer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import {getFakeData} from '../faker/faker.ts';
 import {FakerType} from '../faker/faker.type.enum.ts';
 import {countdown, getTimeoutEnvironmentVariable, progress, unreachable} from '../utils/helper.ts';
